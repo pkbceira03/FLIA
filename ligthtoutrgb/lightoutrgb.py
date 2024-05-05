a973887ae7946d3b79bcfc4548684e4f4d41e295
Marcelo-Adrian <marcelo.10vip@gmail.com>
2024-05-05 15:41:13 -0300

    Tentando fazer o planner funcionar.

--- ligthtoutrgb/lightoutrgb.py
+++ ligthtoutrgb/lightoutrgb.py
@@ -1,49 +1,96 @@
 import sys
+import subprocess
 import os
 
 def read_matrix_from_input():
     matrix = []
-    for line in sys.stdin:  # Lê cada linha da entrada padrão
-        line = line.strip()  # Remove espaços em branco do início e do fim da linha
-        if not line:  # Se a linha estiver vazia, significa que alcançamos o EOF
+    for line in sys.stdin:
+        line = line.strip() 
+        if not line:
             break
         row = list(line.split())
-        # print(row[0][0:2])
-        # print(row[0][2:4])
         lin = []
         for e in range(0, len(row[0]), 2):
             lin.append(row[0][e:e+2])
         matrix.append(lin)
-        # row = line.split()  # Divide a linha nos elementos da matriz
-        # matrix.append(row)  # Adiciona a linha à matriz
     return matrix
 
-# Exemplo de uso
-# print("Digite a matriz (termine com EOF):")
-matriz = read_matrix_from_input()
-# print("Matriz recebida:")
-
-
-# for row in matrix:
-#     print(row)
-
-# abrir o arquivo de problema 
-
-with open ("arquivoProblem", "w") as file:
-    file.write("(define\n\t(problem LIGHTSOUTRGB)\n\t(:domain LIGHTSOUTRGB)")
-    file.write("(:init")
-    for i, linha in enumerate(matriz):
-        file.write(f"\t\tX{i} - column ", end="")
-
-        # #não esta contando o j porque a gente recebe só uma linha 
-        # porque as entradas não tem espaço 
-        #temos que conseguir dividir
-        for j, elemento in enumerate(linha):
-            file.write(f"\ttY{j} - line ", end="")
+def create_problem(matrix):
+    with open ("problemLO.pddl", "w") as file:
+        #Header
+        file.write("(define\n\t(problem LIGHTSOUTRGB)\n\t(:domain LIGHTSOUTRGB)\n\t(:objects")
         
-    #fecha init
-    file.write(")")
-    
-    
-    #fecha define
-    file.write(")")
+        #lines
+        largest_length = 0
+        for sublist in matrix:
+            if len(sublist) > largest_length:
+                largest_length = len(sublist)
+        
+        for i in range(largest_length):
+            file.write(" x{}".format(i))
+        
+        file.write(" - line")
+
+        #column
+        for j, linha in enumerate(matrix):
+            file.write(" y{}".format(j))
+        file.write(" - column)")
+        
+        #init
+        file.write("\n\t(:init\n\t")
+        color_map = {
+            'W': 'white',
+            'R': 'red',
+            'B': 'blue',
+            'G': 'green'
+        }
+
+        for x, row in enumerate(matrix):
+            for y, cell in enumerate(row):
+                color = color_map[cell[1]]
+                file.write(f"({color} x{x} y{y})\n\t")
+        
+        type_map = {
+            '-': 'normal',
+            '|': 'vertical',
+            '_': 'horizontal',
+            '*': 'on-click',
+            '#': 'at-click'
+        }
+
+        for x, row in enumerate(matrix):
+            for y, cell in enumerate(row):
+                cell_type = type_map[cell[0]]
+                file.write(f"({cell_type} x{x} y{y})\n\t")
+        file.write(")\n\t")
+ 
+        #goal
+        file.write("(:goal (and\n\t\t")
+        for x, row in enumerate(matrix):
+            for y, cell in enumerate(row):
+                file.write(f"(white x{x} y{y})\n\t\t")
+        file.write(")\n\t)\n)")
+
+def call_madagascar_planner(domain_file, problem_file):
+    planner_path = '/tmp/dir/software/planners/madagascar/M'
+    command = f'{planner_path} {domain_file} {problem_file}'
+    try:
+        result = subprocess.run(command, shell=True, capture_output=True, text=True, check=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao chamar o planejador Madagascar: {e.stderr}")
+        return None
+
+def main():
+    matrix = read_matrix_from_input()
+    create_problem(matrix)
+    domain_file = 'C:/Users/marce/Documents/Learning/Lighsout/FLIA/ligthtoutrgb/domain.pddl'
+    problem_file = 'C:/Users/marce/Documents/Learning/Lighsout/FLIA/ligthtoutrgb/problem.pddl'
+    # Chamando o planejador Madagascar
+    result_madagascar = call_madagascar_planner(domain_file, problem_file)
+    print("Resultado do planejador Madagascar:")
+    print(result_madagascar)
+
+
+if __name__ == "__main__":
+    main()
